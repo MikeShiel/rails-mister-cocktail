ff823fdd620fd2306e220c9909192f16b306a6b6
Mike Shiel <shiel90@gmail.com>
2017-04-20 17:37:32 +0200

    1st day second attempt

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,28 @@
+class DosesController < ApplicationController
+  def new
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to @cocktail
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose.destroy
+    redirect_to doses_path
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# db/seeds.rb
+puts "starting the seed"
+
+cocktail_json = open("http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list").read
+
+ingredients_list = JSON.parse(cocktail_json)
+
+
+
+ingredients_list["drinks"].each do |hash|
+ Ingredient.create(name: hash["strIngredient1"])
+end
+
+
+
+
+
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
